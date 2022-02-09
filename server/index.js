64db845eef99512c1fac58e8f67ad706b7deafa4
Nazarius21 <nazar.gluhanuk98@gmail.com>
2022-02-09 15:02:52 +0200

    Simple authentication with google API. Use Node.JS, Express, EJS

---
+++ server/index.js
@@ -0,0 +1,91 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 5000;
+const cookieParser = require('cookie-parser');
+
+//Google Auth
+const { OAuth2Client } = require('google-auth-library');
+const CLIENT_ID = '918784054884-5ftpq4jvktr82r0js3oml2bi9ogjnlmp.apps.googleusercontent.com';
+const client = new OAuth2Client(CLIENT_ID);
+
+//Middleware
+app.set('view engine', 'ejs');
+app.use(express.json());
+app.use(cookieParser());
+
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
+app.post('/login', (req, res) => {
+    let token = req.body.token;
+
+    console.log(token);
+    async function verify() {
+        const ticket = await client.verifyIdToken({
+            idToken: token,
+            audience: CLIENT_ID,
+        });
+        const payload = ticket.getPayload();
+        const userId = payload['sub'];
+        console.log(payload)
+    }
+    verify()
+        .then(() => {
+            res.cookie('session-token', token);
+            res.send('success');
+        })
+        .catch(console.error);
+});
+
+app.get('/profile', checkAuthenticated, (req, res) => {
+    let user = req.user;
+    res.render('profile', { user });
+});
+
+app.get('/protected-route', checkAuthenticated, (req, res) => {
+    res.render('protected-route.ejs');
+});
+
+app.get('/logout', (req, res) => {
+    res.clearCookie('session-token');
+    res.redirect('/login');
+});
+
+
+function checkAuthenticated(req, res, next) {
+
+    let token = req.cookies['session-token'];
+
+    let user = {};
+    async function verify() {
+        const ticket = await client.verifyIdToken({
+            idToken: token,
+            audience: CLIENT_ID,
+        });
+
+        const payload = ticket.getPayload();
+        user.name = payload.name;
+        user.email = payload.email;
+        user.picture = payload.picture;
+        user.locale = payload.locale;
+    }
+    verify()
+        .then(() => {
+            req.user = user;
+            next();
+        })
+        .catch(err => {
+            res.redirect('/login');
+            console.error(err);
+        })
+}
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
